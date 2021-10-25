0f851396ee60c8f0d4318f0395864407a0061d67
elizacc <49486233+elizacc@users.noreply.github.com>
2021-10-25 15:11:41 +0300

    Update run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -5,7 +5,7 @@ PROJECT_PATH=`cd ~ && find "$PWD" -name "fse-final-project" -type d`
 if [ -n ${PROJECT_PATH} ]; then 
   sudo docker run  -it -v $PROJECT_PATH:/project \
               --net=host \
-              fse_final_project_image
+              fse-final-project-image
 else
   echo "Unable to find project in /home/username directory"
 fi
