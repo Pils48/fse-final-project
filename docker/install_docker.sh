75a8ec48d91d42cc2242406a77da8501094e73d9
Pils48 <ha.rg@yandex.ru>
2021-10-26 16:06:25 +0300

    Official docker installation.

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-DOCKER_PKGS=`sudo dpkg -la | grep docker`
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-if [ -z "${DOCKER_PKGS}" ]; then
-    # Install snapd if necessary
-    sudo apt update
-    sudo apt install snapd
-    # Install docker from snap
-    sudo snap install docker
-else
-    echo "Docker is already installed on your system"
-fi
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install docker engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+apt-cache madison docker-ce
+
+sudo apt-get install docker-ce=5:20.10.10~3-0~ubuntu-focal docker-ce-cli=5:20.10.10~3-0~ubuntu-focal containerd.io
