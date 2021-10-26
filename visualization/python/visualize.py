81ddf05ead96b27ce08085c09ed488bd8f55e64a
elizacc <49486233+elizacc@users.noreply.github.com>
2021-10-26 15:31:05 +0300

    Update visualize.py

--- visualization/python/visualize.py
+++ visualization/python/visualize.py
@@ -10,20 +10,24 @@ if __name__ == '__main__':
     import argparse
     CMD_PARSER = argparse.ArgumentParser(description="""Visualizing .mat voxel file. """)
     CMD_PARSER.add_argument('-t', '--threshold', metavar='threshold', type=float, default=0.1,
-                            help='voxels with confidence lower than the threshold are not displayed')
+                            help='voxels with confidence lower than the threshold\
+                            are not displayed')
     CMD_PARSER.add_argument('-i', '--index', metavar='index', type=int, default=1,
-                            help='the index of objects in the inputfile that should be rendered (one based)')
+                            help='the index of objects in the inputfile that should be\
+                            rendered (one based)')
     CMD_PARSER.add_argument('filename', metavar='filename', type=str,
                             help='name of .torch or .mat file to be visualized')
     CMD_PARSER.add_argument('-df', '--downsample-factor', metavar='factor', type=int, default=1,
-                            help="downsample objects via a max pooling of step STEPSIZE for efficiency.\
-                            Currently supporting STEPSIZE 1, 2, and 4.")
-    CMD_PARSER.add_argument('-dm', '--downsample-method', metavar='downsample_method', type=str, default='max',
-                            help='downsample method, where mean stands for average pooling and max for max pooling')
+                            help="downsample objects via a max pooling of step STEPSIZE\
+                            for efficiency. Currently supporting STEPSIZE 1, 2, and 4.")
+    CMD_PARSER.add_argument('-dm', '--downsample-method', metavar='downsample_method', type=str,
+                            default='max', help='downsample method, where mean stands for average pooling\
+                            and max for max pooling')
     CMD_PARSER.add_argument('-u', '--uniform-size', metavar='uniform_size', type=float, default=0.9,
                             help='set the size of the voxels to BLOCK_SIZE')
     CMD_PARSER.add_argument('-cm', '--colormap', action="store_true",
-                            help='whether to use a colormap to represent voxel occupancy, or to use a uniform color')
+                            help='whether to use a colormap to represent voxel occupancy,\
+                            or to use a uniform color')
     CMD_PARSER.add_argument('-mc', '--max-component', metavar='max_component', type=int, default=3,
                             help='whether to keep only the maximal connected component,\
                             where voxels of distance no larger than `DISTANCE` are considered connected.\
