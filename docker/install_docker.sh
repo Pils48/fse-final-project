59d23935c2f09bca7050231086a18eab86e4baff
Pils48 <ha.rg@yandex.ru>
2021-10-26 13:31:38 +0300

    Dockerfile refactoring add maintenance for pulled images

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Install snapd if necessary
-sudo apt update
-sudo apt install snapd
+DOCKER_PKGS=`sudo dpkg -la | grep docker`
 
-# Install docker from snap
-sudo snap install docker
+if [ -z "${DOCKER_PKGS}" ]; then
+    # Install snapd if necessary
+    sudo apt update
+    sudo apt install snapd
+    # Install docker from snap
+    sudo snap install docker
+else
+    echo "Docker is already installed on your system"
+fi
