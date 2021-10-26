85245bbaad6bff8e94c99cb20fc3c96c3b55beb6
elizacc <49486233+elizacc@users.noreply.github.com>
2021-10-26 15:20:01 +0300

    Update visualize.py

--- visualization/python/visualize.py
+++ visualization/python/visualize.py
@@ -10,24 +10,24 @@ from util_vtk import visualization
 if __name__ == '__main__':
     import argparse
     CMD_PARSER = argparse.ArgumentParser(description="""Visualizing .mat voxel file. """)
-    cmd_parser.add_argument('-t', '--threshold', metavar='threshold', type=float, default=0.1,
+    CMD_PARSER.add_argument('-t', '--threshold', metavar='threshold', type=float, default=0.1,
                             help='voxels with confidence lower than the threshold are not displayed')
-    cmd_parser.add_argument('-i', '--index', metavar='index', type=int, default=1,
+    CMD_PARSER.add_argument('-i', '--index', metavar='index', type=int, default=1,
                             help='the index of objects in the inputfile that should be rendered (one based)')
-    cmd_parser.add_argument('filename', metavar='filename', type=str,
+    CMD_PARSER.add_argument('filename', metavar='filename', type=str,
                             help='name of .torch or .mat file to be visualized')
-    cmd_parser.add_argument('-df', '--downsample-factor', metavar='factor', type=int, default=1,
+    CMD_PARSER.add_argument('-df', '--downsample-factor', metavar='factor', type=int, default=1,
                             help="downsample objects via a max pooling of step STEPSIZE for efficiency. Currently supporting STEPSIZE 1, 2, and 4.")
-    cmd_parser.add_argument('-dm', '--downsample-method', metavar='downsample_method', type=str, default='max',
+    CMD_PARSER.add_argument('-dm', '--downsample-method', metavar='downsample_method', type=str, default='max',
                             help='downsample method, where mean stands for average pooling and max for max pooling')
-    cmd_parser.add_argument('-u', '--uniform-size', metavar='uniform_size', type=float, default=0.9,
+    CMD_PARSER.add_argument('-u', '--uniform-size', metavar='uniform_size', type=float, default=0.9,
                             help='set the size of the voxels to BLOCK_SIZE')
-    cmd_parser.add_argument('-cm', '--colormap', action="store_true",
+    CMD_PARSER.add_argument('-cm', '--colormap', action="store_true",
                             help='whether to use a colormap to represent voxel occupancy, or to use a uniform color')
-    cmd_parser.add_argument('-mc', '--max-component', metavar='max_component', type=int, default=3,
+    CMD_PARSER.add_argument('-mc', '--max-component', metavar='max_component', type=int, default=3,
                             help='whether to keep only the maximal connected component, where voxels of distance no larger than `DISTANCE` are considered connected. Set to 0 to disable this function.')
 
-    args = cmd_parser.parse_args()
+    args = CMD_PARSER.parse_args()
     filename = args.filename
     matname = 'voxels'
     threshold = args.threshold
