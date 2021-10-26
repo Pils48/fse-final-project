b2af5118467c6a16e101c0c07724d0d951f5564c
elizacc <49486233+elizacc@users.noreply.github.com>
2021-10-26 15:33:46 +0300

    Update visualize.py

--- visualization/python/visualize.py
+++ visualization/python/visualize.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
         VOXELS[np.logical_not(VOXELS_KEEP)] = 0
 
     # downsample if needed
-    if DOWNSAMPLE_FACTOR > 1:
+    if _FACTOR > 1:
         print("==> Performing downsample: factor: "+str(FACTOR)+" method: "+METHOD)
         VOXELS = downsample(VOXELS, FACTOR, method=METHOD)
         print("Done")
